06d9fe61a3b81de1d05321f39a1d51354540b020
N. Fajri <fajriajjha@gmail.com>
2020-06-02 19:25:53 +0700

    enable middleware cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 (new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
     dirname(__DIR__)
@@ -87,6 +87,10 @@ $app->configure('swagger-lume');
 //     'auth' => App\Http\Middleware\Authenticate::class,
 // ]);
 
+$app->middleware([
+    App\Http\Middleware\CorsMiddleware::class
+]);
+
 /*
 |--------------------------------------------------------------------------
 | Register Service Providers
@@ -120,7 +124,7 @@ $app->register(\SwaggerLume\ServiceProvider::class);
 $app->router->group([
     'namespace' => 'App\Http\Controllers',
 ], function ($router) {
-    require __DIR__.'/../routes/web.php';
+    require __DIR__ . '/../routes/web.php';
 });
 
 return $app;
